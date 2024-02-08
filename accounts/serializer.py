d2859da16f7466638131961943a2ac5da01ece89
Omarmoatz <omar.raouf.002@gmail.com>
2024-02-08 02:44:18 +0200

    add password confirmation

--- accounts/serializer.py
+++ accounts/serializer.py
@@ -4,14 +4,23 @@ from django.contrib.auth.models import User
 class SignUpSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['first_name','last_name','email','password']
+        fields = ['username','email','password','password2']
         extra_kwargs = {
-            'first_name':{'required':True},
-            'last_name':{'required':True},
+            'username':{'required':True},
             'email':{'required':True},
-            'password':{'required':True,'min_length':8}
+            'password':{'required':True,'min_length':8},
+            'password2':{'required':True},
         }
 
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError("Passwords do not match.")
+        return attrs
+
+    def create(self, validated_data):
+        validated_data.pop('password2')
+        return super().create(validated_data)
+    
 class InfoSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
