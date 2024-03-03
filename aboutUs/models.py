80518d0eeca92a476a8c6a31599deb0b3bae0f09
Omarmoatz <omar.raouf.002@gmail.com>
2024-03-03 15:31:33 +0200

    deign about us models

--- aboutUs/models.py
+++ aboutUs/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Company(models.Model):
+    name = models.CharField( max_length=100,default='default_name')
+    logo = models.ImageField( upload_to='company_logo')
+    email_link = models.URLField( max_length=200, blank=True, null=True)
+    whatsapp_link = models.URLField( max_length=200, blank=True, null=True)
+    facebook_link = models.URLField( max_length=200, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Company info'
+        verbose_name_plural = 'Company info'
+class Article(models.Model):
+    title = models.CharField( max_length=100,default='default_title')
+    image = models.ImageField( upload_to='article_image')
+    content = models.TextField(max_length=9000000, default='default_content')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Article'
+        verbose_name_plural = 'Articls'
--- project/settings.py
+++ project/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
     # my apps
     'accounts',
+    'aboutUs',
 ]
 
 MIDDLEWARE = [
