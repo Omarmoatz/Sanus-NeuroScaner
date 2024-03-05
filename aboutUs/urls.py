676e1c5528576129010d1f0c4ae735a1b3a818d1
Omarmoatz <omar.raouf.002@gmail.com>
2024-03-05 23:01:45 +0200

    add get company info function

--- aboutUs/urls.py
+++ aboutUs/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
+from .views import company_info
+
+#  aboutUs/
 urlpatterns = [
-    
+    path('',company_info)
 ]
--- aboutUs/views.py
+++ aboutUs/views.py
@@ -1,7 +1,11 @@
 from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 from .models import Company,Article
+from .serializers import CompanySerializer,ArticleSerializer
 
-
-
-
+@api_view(['GET'])
+def company_info(request):
+    info = Company.objects.last()
+    serializer = CompanySerializer(info).data
+    return Response({'data':serializer})
