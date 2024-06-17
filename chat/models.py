5dd4f0978dab8a21224bc19d0f9616074a5dfb22
Omarmoatz <omar.raouf.002@gmail.com>
2024-06-17 18:32:46 +0300

    add chat list api

--- chat/models.py
+++ chat/models.py
@@ -3,8 +3,8 @@ from django.db import models
 from accounts.models import CustomUser
 
 class ChatMessage(models.Model):
-    sender = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='sender')
-    receiver = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='receiver')
+    sender = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='sent_messege')
+    receiver = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='received_messege')
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     is_read = models.BooleanField(default=False)
--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from django.db.models import Subquery , Q , OuterRef
 
+from .models import ChatMessage, CustomUser
+from .serializers import ChatMessageSerializer
 
+class ChatList(generics.ListAPIView):
+    # finding all chats between me and other users
+    serializer_class = ChatMessageSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+
+        # finding last messege between me and other users and order them then put there id in list 
+        latest_messege = ChatMessage.objects.filter(
+            Q( sender= OuterRef('id'), recevier= user ) |
+            Q( sender= user, receiver= OuterRef('id') )
+        ).order_by('-id')[:1].values_list('id', flat=True)
+
+        # finding all the users that i have sent or received messege from them 
+        related_users = CustomUser.objects.filter(
+            Q( sent_messege__receiver= user ) |
+            Q( received_messege__sender= user )
+        ).distinct().annotate(
+            last_messege = Subquery(latest_messege)
+        ).values_list('last_messege' , flat=True )
+
+        # filter chats to bring all ids with the following subquery 
+        messege = ChatMessage.objects.filter(
+            id__in = Subquery(related_users)
+        ).order_by('-id')
+
+        return messege
+    
